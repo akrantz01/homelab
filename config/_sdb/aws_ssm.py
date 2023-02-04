1d25459db012464b8aea9518ff6803af5c65ace2
Alex Krantz <alex@krantz.dev>
2023-02-04 15:29:13 -0800

    feat(config): testing custom sdb module profiles

--- config/_sdb/aws_ssm.py
+++ config/_sdb/aws_ssm.py
@@ -9,7 +9,28 @@ AWS SSM Parameter Store SDB Module
 This module allows access to AWS SSM Parameter Store using an ``sdb://`` URI.
 """
 
+import logging
+
+import salt.exceptions
+
+try:
+  import boto3
+  import botocore.exceptions
+
+  logging.getLogger("boto3").setLevel(logging.CRITICAL)
+except ImportError:
+  pass
+
+log = logging.getLogger(__name__)
+
 __func_alias__ = {"set_": "set"}
+
+def __virtual__():
+  """
+  Only load if boto3 libraries exist and if boto libraries are greater than
+  a given version.
+  """
+  return salt.utils.versions.check_boto_reqs()
 
 def set_(key, value, profile=None):
   """
@@ -22,6 +43,8 @@ def get(key, profile=None):
   """
   Get a value from the AWS SSM Parameter Store
   """
+  log.info(key)
+  log.info(profile)
   return True
 
 
