4345e2d860678eb5285e413264210c9db9ed034c
Alex Krantz <alex@krantz.dev>
2023-07-01 00:26:33 -0700

    feat(user): output ses smtp password

--- modules/user/outputs.tf
+++ modules/user/outputs.tf
@@ -7,3 +7,8 @@ output "secret_access_key" {
   value       = aws_iam_access_key.user.secret
   description = "The AWS secret access key"
 }
+
+output "smtp_password" {
+  value       = aws_iam_access_key.user.ses_smtp_password_v4
+  description = "The AWS SES SMTP password"
+}
