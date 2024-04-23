79a8f32280919a3355176322febfaeb081a600cd
Alex Krantz <alex@krantz.dev>
2024-04-23 10:47:41 -0700

    Add nix flake for development

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "My homelab configuration";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            just
+            opentofu
+            tflint
+            yamllint
+          ];
+          shellHook = ''
+            alias j=just
+          '';
+        };
+      };
+    };
+}
