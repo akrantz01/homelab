57c99942e6707d0449648c2402e028c07baac4c1
Alex Krantz <alex@krantz.dev>
2024-04-23 17:01:23 -0700

    Fix linting and formatting modules

--- terraform/providers/aws.tf
+++ terraform/providers/aws.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.aws_region
 }
 
+# tflint-ignore: terraform_standard_module_structure
 variable "aws_region" {
   type        = string
   description = "The region to deploy to for the AWS provider"
--- terraform/providers/cloudflare.tf
+++ terraform/providers/cloudflare.tf
@@ -2,6 +2,7 @@ provider "cloudflare" {
   api_token = var.cloudflare_token
 }
 
+# tflint-ignore: terraform_standard_module_structure
 variable "cloudflare_token" {
   type        = string
   description = "The token for authenticating with the Cloudflare API"
