cceb4e7571c5e308721f6acdb9de0bb81824c9a2
Alex Krantz <alex@krantz.dev>
2024-06-30 01:34:42 -0700

    Add static network configuration

---
+++ systems/hosts/krantz/network-configuration.nix
@@ -0,0 +1,49 @@
+# Sets up the systems networking using systemd-networkd
+# with static addresses and routes.
+
+{ ... }:
+
+let
+  address = addr: { addressConfig.Address = addr; };
+in
+{
+  # Uncomment to enable debug logging
+  #systemd.services."systemd-networkd".environment.SYSTEMD_LOG_LEVEL = "debug";
+
+  # Prefer systemd-networkd for networking for better reliability
+  systemd.network.enable = true;
+  networking.useDHCP = false;
+
+  # Set WAN routing configuration
+  systemd.network.networks."10-wan" = {
+    name = "enp35s0";
+
+    # Ensure everything is static
+    DHCP = "no";
+    networkConfig = {
+      IPv6AcceptRA = "no";
+    };
+
+    addresses = [
+      (address "23.139.82.37/24")
+      (address "23.139.82.253/24")
+      (address "2602:fb89:1:25::1/64")
+      (address "fe80::aaa1:59ff:fec0:7e0c/64")
+    ];
+
+    routes = [
+      { routeConfig.Gateway = "23.139.82.1"; }
+      { routeConfig = { Gateway = "2602:fb89:1::1"; GatewayOnLink = true; }; }
+    ];
+
+    dns = [
+      "1.1.1.1"
+      "2606:4700:4700::1111"
+      "1.0.0.1"
+      "2606:4700:4700::1001"
+    ];
+
+    # Make the routes on this interface a dependency for network-online.target.
+    linkConfig.RequiredForOnline = "routable";
+  };
+}
