3808e9a44dc497dbcc65182fd9e87384b333327a
Alex Krantz <alex@krantz.dev>
2024-07-20 13:23:43 -0700

    Trust garnix cache on nixos

--- common/nix.nix
+++ common/nix.nix
@@ -6,6 +6,9 @@
     auto-optimise-store = true;
 
     extra-experimental-features = [ "nix-command" "flakes" ];
+
+    substituters = [ "https://cache.garnix.io" ];
+    trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
   };
 
   # Prune the nix store weekly, removing things older than 30 days
