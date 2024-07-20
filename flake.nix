e47a7baa48b7feaa75215de1b11c4318c4306ed7
Alex Krantz <alex@krantz.dev>
2024-07-20 13:01:51 -0700

    Replace flake-parts with pure nix

--- flake.nix
+++ flake.nix
@@ -2,48 +2,43 @@
   description = "My homelab configuration";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, sops-nix, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+  outputs = inputs@{ self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      nixosConfigurations.krantz = nixpkgs.lib.nixosSystem {
+        inherit system;
 
-      flake.nixosConfigurations =
-        let
-          system = "x86_64-linux";
+        modules = [
+          "${self}/hosts/krantz/configuration.nix"
+          # "${self}/common"
+          # "${self}/components"
+          {
+            _module.args = { inherit inputs; };
+          }
+        ];
+      };
 
-          specialArgs = { inherit self system; };
-        in
-        {
-          krantz = nixpkgs.lib.nixosSystem {
-            inherit specialArgs;
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          bashInteractive
+          copier
+          just
+          opentofu
+          sops
+          terragrunt
+          tflint
+          yamllint
+        ];
 
-            modules = [
-              "${self}/hosts/krantz/configuration.nix"
-              "${self}/common"
-              "${self}/components"
-            ];
-          };
-        };
-
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            bashInteractive
-            copier
-            just
-            opentofu
-            sops
-            terragrunt
-            tflint
-            yamllint
-          ];
-          shellHook = ''
-            alias j=just
-          '';
-        };
+        shellHook = ''
+          alias j=just
+        '';
       };
     };
 
