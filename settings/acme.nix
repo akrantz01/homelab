1e85dcf4778508d82b2efe90dc61dbda1f225a13
Alex Krantz <alex@krantz.dev>
2024-07-21 20:03:41 -0700

    Fix ACME credentials

--- settings/acme.nix
+++ settings/acme.nix
@@ -8,8 +8,8 @@
     name = "cloudflare";
 
     credentials = {
-      CF_DNS_API_TOKEN = "acme/dns_api_token";
-      CF_ZONE_API_TOKEN = "acme/zone_api_token";
+      CF_DNS_API_TOKEN_FILE = "acme/dns_api_token";
+      CF_ZONE_API_TOKEN_FILE = "acme/zone_api_token";
     };
   };
 }
