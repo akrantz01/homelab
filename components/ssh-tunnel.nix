2aef2c69f10b7ea53e8f35b63a0cc4142ce1c2e0
Alex Krantz <alex@krantz.dev>
2024-07-28 00:22:04 -0700

    Make ssh only listen locally

--- components/ssh-tunnel.nix
+++ components/ssh-tunnel.nix
@@ -28,6 +28,12 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    services.openssh.listenAddresses = [
+      {
+        addr = "127.0.0.1";
+        port = 22;
+      }
+    ];
     services.openssh.settings.Macs = [
       "hmac-sha2-512-etm@openssh.com"
       "hmac-sha2-256-etm@openssh.com"
