0823d507f94d474dfb92499478091309fd9544ed
Alex Krantz <alex@krantz.dev>
2024-08-07 20:57:26 -0700

    Add dependencies for mounts

--- components/backblaze.nix
+++ components/backblaze.nix
@@ -104,6 +104,8 @@ in {
           "buffer_size=128Mi"
           "vfs_read_ahead=512Mi"
         ];
+        after = ["network-online.target" "systemd-tmpfiles-setup.service"];
+        requires = ["systemd-tmpfiles-setup.service"];
       })
       flattenedMounts;
 
