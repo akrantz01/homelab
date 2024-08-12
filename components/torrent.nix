705d7e231e77b9ff756a5c3845d7612dde7fce92
Alex Krantz <alex@krantz.dev>
2024-08-11 23:45:30 -0700

    Add initial pass of flood UI

--- components/torrent.nix
+++ components/torrent.nix
@@ -38,5 +38,35 @@ in {
       unitConfig.JoinsNamespaceOf = "netns@vpn.service";
       serviceConfig.PrivateNetwork = true;
     };
+
+    systemd.services.flood = {
+      description = "Flood web UI for Transmission";
+      after = ["netowrk.target"];
+      wantedBy = ["multi-user.target"];
+
+      serviceConfig = {
+        Type = "simple";
+        KillMode = "process";
+
+        ExecStart = builtins.concatStringsSep " " [
+          "${pkgs-unstable.flood}/bin/flood"
+          "--rundir=/var/lib/flood"
+          "--auth=none"
+          "--port=3563"
+          "--allowedpath=${config.services.transmission.settings.download-dir}"
+          "--allowedpath=${config.services.transmission.settings.incomplete-dir}"
+          "--trurl=unix:/run/transmission/rpc.sock"
+        ];
+
+        User = "flood";
+        Group = "flood";
+        DynamicUser = true;
+
+        Restart = "on-failure";
+        RestartSec = 3;
+
+        StateDirectory = "flood";
+      };
+    };
   };
 }
