74ef7742e6bd33be35307cf885e081ac20161876
Alex Krantz <alex@krantz.dev>
2024-12-13 15:16:32 -0800

    Remove unused import for sops-nix

--- flake.nix
+++ flake.nix
@@ -10,10 +10,7 @@
 
     sops-nix = {
       url = "github:Mic92/sops-nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        nixpkgs-stable.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
