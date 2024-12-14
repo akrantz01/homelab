964e33be7f1aef1c4a63309fa5552e5b6f8c6381
Alex Krantz <alex@krantz.dev>
2024-12-14 13:06:29 -0800

    Enable smtp for mealie

--- components/mealie/default.nix
+++ components/mealie/default.nix
@@ -84,6 +84,34 @@ in {
         description = "The OpenAI model to use";
       };
     };
+
+    smtp = {
+      enable = lib.mkEnableOption "Enable SMTP for sending emails";
+
+      host = extra.mkSecretOption "SMTP host" "mealie/smtp/host";
+      port = extra.mkSecretOption "SMTP port" "mealie/smtp/port";
+      username = extra.mkSecretOption "SMTP username" "mealie/smtp/username";
+      password = extra.mkSecretOption "SMTP password" "mealie/smtp/password";
+
+      security = lib.mkOption {
+        type = lib.types.str;
+        default = "TLS";
+        description = "The security method to use for the SMTP connection (TLS, SSL, NONE)";
+      };
+
+      from = {
+        name = lib.mkOption {
+          type = lib.types.str;
+          default = "Recipes";
+          description = "The name to use for the from field in emails";
+        };
+        address = lib.mkOption {
+          type = lib.types.str;
+          default = "no-reply@${cfg.domain}";
+          description = "The email to use for the from field in emails";
+        };
+      };
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -134,9 +162,12 @@ in {
             OIDC_USER_GROUP = cfg.oidc.groups.user;
             OIDC_ADMIN_GROUP = cfg.oidc.groups.admin;
           })
-        (lib.optionalAttrs cfg.openai.enable
+        (lib.optionalAttrs cfg.openai.enable {OPENAI_MODEL = cfg.openai.model;})
+        (lib.optionalAttrs cfg.smtp.enable
           {
-            OPENAI_MODEL = cfg.openai.model;
+            SMTP_AUTH_STRATEGY = cfg.smtp.security;
+            SMTP_FROM_NAME = cfg.smtp.from.name;
+            SMTP_FROM_EMAIL = cfg.smtp.from.address;
           })
       ];
     };
@@ -153,6 +184,10 @@ in {
     sops.secrets = {
       "mealie/openai_api_key" = mkSecret cfg.openai.apiKey "openai_api_key";
       "mealie/oidc_client_secret" = mkSecret cfg.oidc.clientSecret "oidc_client_secret";
+      "mealie/smtp/host" = mkSecret cfg.smtp.host "smtp_host";
+      "mealie/smtp/port" = mkSecret cfg.smtp.port "smtp_port";
+      "mealie/smtp/username" = mkSecret cfg.smtp.username "smtp_user";
+      "mealie/smtp/password" = mkSecret cfg.smtp.password "smtp_password";
     };
 
     components.reverseProxy.hosts.${cfg.domain}.locations."/" = {
--- hosts/krantz/default.nix
+++ hosts/krantz/default.nix
@@ -89,6 +89,7 @@
         configurationUrl = "https://oauth.id.jumpcloud.com/.well-known/openid-configuration";
         clientId = "13f91be7-8a05-45d5-bed3-2131b55dcc33";
       };
+      smtp.enable = true;
     };
     miniflux = {
       enable = true;
