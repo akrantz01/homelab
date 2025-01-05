b3dcfb847ef343367ea074bc4123a5b5c0829aca
Alex Krantz <alex@krantz.dev>
2025-01-04 16:04:31 -0800

    Create cloudfront distribution for cms assets

---
+++ terraform/applications/cms/cdn.tf
@@ -0,0 +1,48 @@
+locals {
+  default_origin = module.bucket.domain_name
+}
+
+data "aws_cloudfront_cache_policy" "default" {
+  name = "Managed-CachingOptimized"
+}
+
+data "aws_cloudfront_response_headers_policy" "default" {
+  name = "Managed-CORS-and-SecurityHeadersPolicy"
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled = true
+  comment = "CDN for cms.krantz.dev assets"
+
+  http_version    = "http2and3"
+  is_ipv6_enabled = true
+
+  origin {
+    origin_id   = local.default_origin
+    domain_name = module.bucket.domain_name
+  }
+
+  default_cache_behavior {
+    target_origin_id = local.default_origin
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+    compress        = true
+
+    cache_policy_id            = data.aws_cloudfront_cache_policy.default.id
+    response_headers_policy_id = data.aws_cloudfront_response_headers_policy.default.id
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
--- terraform/modules/bucket/outputs.tf
+++ terraform/modules/bucket/outputs.tf
@@ -8,6 +8,11 @@ output "arn" {
   description = "The resulting bucket ARN"
 }
 
+output "domain_name" {
+  value       = aws_s3_bucket.bucket.bucket_regional_domain_name
+  description = "The resulting bucket domain name"
+}
+
 output "policy" {
   value       = data.aws_iam_policy_document.policy.json
   description = "The policy for allowing read and write access to the bucket"
