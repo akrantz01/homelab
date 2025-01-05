0ad4acd77eb6fc20ee4a450ce2096b370983dc2e
Alex Krantz <alex@krantz.dev>
2025-01-04 16:36:32 -0800

    Apply origin access control to cms assets origin

--- terraform/applications/cms/cdn.tf
+++ terraform/applications/cms/cdn.tf
@@ -10,6 +10,15 @@ data "aws_cloudfront_response_headers_policy" "default" {
   name = "Managed-CORS-and-SecurityHeadersPolicy"
 }
 
+resource "aws_cloudfront_origin_access_control" "cms" {
+  name        = "CmsBucket"
+  description = "Restrict access to the CMS asset bucket"
+
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
 resource "aws_cloudfront_distribution" "cdn" {
   enabled = true
   comment = "CDN for cms.krantz.dev assets"
@@ -20,6 +29,8 @@ resource "aws_cloudfront_distribution" "cdn" {
   origin {
     origin_id   = local.default_origin
     domain_name = module.bucket.domain_name
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.cms.id
   }
 
   default_cache_behavior {
