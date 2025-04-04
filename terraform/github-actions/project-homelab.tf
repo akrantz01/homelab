cfc143313e91a27320d01358f286294c175d02a2
Alex Krantz <alex@krantz.dev>
2025-04-03 21:37:54 -0700

    Fix naming for mulitple github actions projects

--- terraform/github-actions/project-homelab.tf
+++ terraform/github-actions/project-homelab.tf
@@ -1,7 +1,3 @@
-data "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-}
-
 resource "aws_iam_role" "homelab" {
   name        = "GitHubActionsHomelab"
   description = "The role assumed by the akrantz01/homelab repository on GitHub."
