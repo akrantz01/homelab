fd065bf7cf52bf53f74cf572b225a6a6bf08315d
Alex Krantz <alex@krantz.dev>
2025-04-03 21:50:13 -0700

    Fix homelab assume role secret name

--- terraform/github-actions/secrets.tf
+++ terraform/github-actions/secrets.tf
@@ -1,4 +1,4 @@
-resource "github_actions_secret" "aws_assume_role" {
+resource "github_actions_secret" "homelab_aws_assume_role" {
   repository      = "homelab"
   secret_name     = "AWS_ASSUME_ROLE_ARN"
   plaintext_value = aws_iam_role.homelab.arn
