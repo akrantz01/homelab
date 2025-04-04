de94775e1a17a570e462f200adc13df020743a53
Alex Krantz <alex@krantz.dev>
2025-04-03 21:49:44 -0700

    Add tailfed github actions secret

--- terraform/github-actions/secrets.tf
+++ terraform/github-actions/secrets.tf
@@ -3,3 +3,10 @@ resource "github_actions_secret" "aws_assume_role" {
   secret_name     = "AWS_ASSUME_ROLE_ARN"
   plaintext_value = aws_iam_role.homelab.arn
 }
+
+resource "github_actions_secret" "tailfed_aws_assume_role" {
+  repository      = "tailfed"
+  secret_name     = "AWS_ASSUME_ROLE_ARN"
+  plaintext_value = aws_iam_role.tailfed.arn
+}
+
