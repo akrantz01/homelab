a725ef8bb1c3e02dbca6bfd3b7d9ea645c5f78b1
Alexander Krantz <alex@krantz.dev>
2025-04-06 03:05:36 -0700

    Switch to S3 bucket for tailfed artifacts (#210)

---
+++ terraform/projects/tailfed/s3.tf
@@ -0,0 +1,45 @@
+resource "aws_s3_bucket" "artifacts" {
+  bucket = "tailfed-artifacts"
+}
+
+resource "aws_s3_bucket_ownership_controls" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "artifacts" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.artifacts,
+    aws_s3_bucket_public_access_block.artifacts
+  ]
+
+  bucket = aws_s3_bucket.artifacts.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  policy = data.aws_iam_policy_document.artifacts.json
+}
+
+data "aws_iam_policy_document" "artifacts" {
+  statement {
+    sid       = "AllowGetObject"
+    effect    = "Allow"
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.artifacts.arn}/*"]
+  }
+}
