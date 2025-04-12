c3d5b53453812e8ac88ce5ccf95571c3981286f9
Alex Krantz <alex@krantz.dev>
2025-04-12 12:53:30 -0700

    Revert "Rename old tailfed artifact resource storage" Move renamed tailfed artifact resources back
    This reverts commit 463348bcea7fcfc578f43f3dd934d1fc284de73e.

--- terraform/projects/tailfed/s3.tf
+++ terraform/projects/tailfed/s3.tf
@@ -1,17 +1,27 @@
-resource "aws_s3_bucket" "global_artifacts" {
+resource "aws_s3_bucket" "artifacts" {
   bucket = "tailfed-artifacts"
 }
 
-resource "aws_s3_bucket_ownership_controls" "global_artifacts" {
-  bucket = aws_s3_bucket.global_artifacts.id
+moved {
+  from = aws_s3_bucket.global_artifacts
+  to = aws_s3_bucket.artifacts
+}
+
+resource "aws_s3_bucket_ownership_controls" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
 
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "global_artifacts" {
-  bucket = aws_s3_bucket.global_artifacts.id
+moved {
+  from = aws_s3_bucket_ownership_controls.global_artifacts
+  to = aws_s3_bucket_ownership_controls.artifacts
+}
+
+resource "aws_s3_bucket_public_access_block" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -19,28 +29,43 @@ resource "aws_s3_bucket_public_access_block" "global_artifacts" {
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "global_artifacts" {
+moved {
+  from = aws_s3_bucket_public_access_block.global_artifacts
+  to = aws_s3_bucket_public_access_block.artifacts
+}
+
+resource "aws_s3_bucket_acl" "artifacts" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.global_artifacts,
-    aws_s3_bucket_public_access_block.global_artifacts
+    aws_s3_bucket_ownership_controls.artifacts,
+    aws_s3_bucket_public_access_block.artifacts
   ]
 
-  bucket = aws_s3_bucket.global_artifacts.id
+  bucket = aws_s3_bucket.artifacts.id
   acl    = "public-read"
 }
 
-resource "aws_s3_bucket_policy" "global_artifacts" {
-  bucket = aws_s3_bucket.global_artifacts.id
-
-  policy = data.aws_iam_policy_document.global_artifacts.json
+moved {
+  from = aws_s3_bucket_acl.global_artifacts
+  to = aws_s3_bucket_acl.artifacts
 }
 
-data "aws_iam_policy_document" "global_artifacts" {
+resource "aws_s3_bucket_policy" "artifacts" {
+  bucket = aws_s3_bucket.artifacts.id
+
+  policy = data.aws_iam_policy_document.artifacts.json
+}
+
+moved {
+  from = aws_s3_bucket_policy.global_artifacts
+  to = aws_s3_bucket_policy.artifacts
+}
+
+data "aws_iam_policy_document" "artifacts" {
   statement {
     sid       = "AllowGetObject"
     effect    = "Allow"
     actions   = ["s3:GetObject"]
-    resources = ["${aws_s3_bucket.global_artifacts.arn}/*"]
+    resources = ["${aws_s3_bucket.artifacts.arn}/*"]
     principals {
       type        = "AWS"
       identifiers = ["*"]
