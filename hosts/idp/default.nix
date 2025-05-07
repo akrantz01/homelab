523b3ceffb99bdcaf2495f5b040f4a62324394b4
Alex Krantz <alex@krantz.dev>
2025-05-06 22:35:47 -0700

    Enable continuous deployment daemon for idp

--- hosts/idp/default.nix
+++ hosts/idp/default.nix
@@ -6,7 +6,9 @@
   ec2.efi = true;
   time.timeZone = "America/Montreal";
 
-  components = {};
+  components = {
+    continuousDeployment.enable = true;
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
