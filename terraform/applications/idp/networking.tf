9204cf499b401bde80cebd547dd5db781dae261e
Alex Krantz <alex@krantz.dev>
2025-05-10 22:45:26 -0700

    Allow HTTP/S access

--- terraform/applications/idp/networking.tf
+++ terraform/applications/idp/networking.tf
@@ -12,6 +12,30 @@ resource "aws_security_group" "idp" {
   vpc_id = var.vpc_id
 }
 
+resource "aws_security_group_rule" "http" {
+  security_group_id = aws_security_group.idp.id
+  type              = "ingress"
+
+  protocol  = "tcp"
+  from_port = 80
+  to_port   = 80
+
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+}
+
+resource "aws_security_group_rule" "https" {
+  security_group_id = aws_security_group.idp.id
+  type              = "ingress"
+
+  protocol  = "tcp"
+  from_port = 443
+  to_port   = 443
+
+  cidr_blocks      = ["0.0.0.0/0"]
+  ipv6_cidr_blocks = ["::/0"]
+}
+
 resource "aws_security_group_rule" "egress" {
   security_group_id = aws_security_group.idp.id
   type              = "egress"
