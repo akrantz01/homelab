9813330ec23347d716cd593f8f024e6319df90fd
Alex Krantz <alex@krantz.dev>
2025-10-11 14:19:06 -0700

    Remove old backups bucket

--- hosts/krantz/default.nix
+++ hosts/krantz/default.nix
@@ -70,15 +70,12 @@
     };
     backblaze = {
       enable = true;
-      buckets = {
-        watch-krantz-dev = {
-          chunked = true;
-          paths = {
-            "/mnt/movies" = "movies";
-            "/mnt/tv" = "tv";
-          };
+      buckets.watch-krantz-dev = {
+        chunked = true;
+        paths = {
+          "/mnt/movies" = "movies";
+          "/mnt/tv" = "tv";
         };
-        primary-krantz-dev-backups.paths."/mnt/backups/postgres" = "postgres";
       };
     };
 
