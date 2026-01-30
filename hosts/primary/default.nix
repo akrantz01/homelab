72badf3d5a209d057066e6b347c51bdc776de964
Alex Krantz <alex@krantz.dev>
2026-01-29 19:40:40 -0800

    Disable all database-using services on primary

--- hosts/primary/default.nix
+++ hosts/primary/default.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   imports = [
     ./disk-config.nix
     ./hardware-configuration.nix
@@ -159,6 +159,8 @@
       };
     };
   };
+
+  systemd.services = builtins.listToAttrs (lib.map (service: lib.nameValuePair service {enable = false;}) ["atuin" "mealie" "miniflux" "n8n" "vaultwarden"]);
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
