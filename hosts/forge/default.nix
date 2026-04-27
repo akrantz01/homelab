2b3893e96718a86901e1dd9112639e28404dd09c
Alex Krantz <alex@krantz.dev>
2026-04-26 19:04:46 -0700

    Enable continuous deployment daemon for forge

--- hosts/forge/default.nix
+++ hosts/forge/default.nix
@@ -6,6 +6,10 @@
 
   time.timeZone = "America/Montreal";
 
+  components = {
+    continuousDeployment.enable = true;
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
